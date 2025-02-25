be7668d015496410a4a1fc798795ad541e486f61
tristan.alhabas@gmail.com <tristan.alhabas@gmail.com>
2025-02-25 16:24:31 +0700

    Init

--- lib/pages/registration/sign_in.dart
+++ lib/pages/registration/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:eco_style/themes/color_pallete.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
@@ -5,6 +6,18 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: ColorPallete.white,
+      body: Center(
+        child: Text(
+          "Sign-In Page On Development.",
+          style: TextStyle(
+              color: ColorPallete.darkBrown,
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Montserrat'),
+        ),
+      ),
+    );
   }
 }
